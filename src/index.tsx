3be79da536769ca0dbe14c7ccfb35d41bde39ae0
kkw855 <kkw855@gmail.com>
2025-05-09 11:50:23 +0900

    Tailwind default font "DM Sans" 로 설정

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ if (!root) throw new Error('No root element found')
 
 createRoot(root).render(
     <StrictMode>
-      <h1 className="text-amber-400">HI</h1>
+      <h1 className="text-amber-400 text-2xl">Hello World!</h1>
+      <h1 className="text-amber-400 text-2xl font-mono">Hello World!</h1>
     </StrictMode>,
 )
